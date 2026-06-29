957afe95bfd09c1f5b123d4cee3d22a55a419c73
AkbarshoxOtaboev <otaboevakbar96@gmail.com>
2026-06-29 10:32:40 +0500

    first commit

---
+++ src/test/java/uz/urspi/newurspi/NewurspiApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.urspi.newurspi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewurspiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
